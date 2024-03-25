ffe4b0170cafcec892d29b3267383352215aac44
Stiven Diaz <steven_el22@hotmail.com>
2024-03-25 15:08:27 -0700

    changed repo,final proyect

--- src/main/java/backend/proyect/global/proyect_global/Player/PlayerService.java
+++ src/main/java/backend/proyect/global/proyect_global/Player/PlayerService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+@SuppressWarnings("null")
 @Service
 public class PlayerService {
 
